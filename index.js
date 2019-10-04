0793882e00ecc7081bc5a1e522531415b05e5d03
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2019-10-04 08:05:26 -0300

    chore: add hook support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
     extends: [
       "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+      "plugin:react-hooks",
       "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
       "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
